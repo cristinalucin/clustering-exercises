b94bc567a4443d3b415e3b89fc466aa41cee9017
Cristina Lucin <cristinajlucin@gmail.com>
2022-11-28 16:02:30 -0600

    updated explore file

--- wrangle.py
+++ wrangle.py
@@ -87,7 +87,7 @@ def summarize(df):
     print(nulls_by_row(df))
     print('=====================================================')
     
-##-------------------Null Values-----------------##
+##-------------------Prepare-----------------##
     
 def nulls_by_col(df):
     num_missing = df.isnull().sum()
@@ -115,3 +115,58 @@ def handle_missing_values(df, prop_required_columns=0.5, prop_required_rows=0.75
     df = df.dropna(axis=0, thresh=row_threshold)
     return df
 
+def prepare_zillow(df):
+    '''This function imputes missing values when applicable, and drops columns with too much missing data. Finally,
+    it utilizes another function to handle the missing values based on the proportion of rows and column values missing'''
+    df.fullbathcnt = df.fullbathcnt.fillna(0)
+    df.pooltypeid2 = df.pooltypeid2.fillna(0)
+    df.pooltypeid10 = df.pooltypeid10.fillna(0)
+    df.pooltypeid7 = df.pooltypeid7.fillna(0)
+    df.fireplacecnt = df.fireplacecnt.fillna(0)
+    df.decktypeid = df.decktypeid.fillna(0)
+    df.poolcnt = df.poolcnt.fillna(0)
+    df.hashottuborspa = df.hashottuborspa.fillna(0)
+    df.typeconstructiondesc = df.typeconstructiondesc.fillna('None')
+    df.fireplaceflag = df.fireplaceflag.fillna(0)
+    df.threequarterbathnbr = df.threequarterbathnbr.fillna(0)
+    df.taxdelinquencyyear = df.taxdelinquencyyear.fillna(99999)
+    df.taxdelinquencyflag = df.taxdelinquencyflag.fillna('N')
+    df.calculatedbathnbr = df.calculatedbathnbr.fillna(0)
+    df.basementsqft = df.basementsqft.fillna(0)
+    df.numberofstories.value_counts(dropna=False)
+    
+    df = handle_missing_values(df, prop_required_columns=.5, prop_required_rows=.75)
+    
+    # Relabeling FIPS data
+    df['county'] = df.fips.replace({6037:'Los Angeles',
+                       6059:'Orange',
+                       6111:'Ventura'})
+    
+    #Drop columns with too many null values/extraneous information
+    df = df.drop(columns=['buildingqualitytypeid','propertyzoningdesc','unitcnt','heatingorsystemdesc','id', 'id.1','heatingorsystemtypeid'])
+    
+    #Replace a whitespace sequence or empty with a NaN value and reassign this manipulation to df. 
+    df = df.replace(r'^\s*$', np.nan, regex=True)
+    
+    #Drop remainder of rows with null values
+    df = df.dropna()
+    
+    return df
+
+###-----------------------Train/Test Split------------------###
+
+
+def train_validate_test_split(df, seed=123):
+    '''
+    This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 15% of the original dataset, validate is .1765*.85= 15% of the 
+    original dataset, and train is 75% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
+    '''
+    train_validate, test = train_test_split(df, test_size=0.15, 
+                                            random_state=seed)
+    train, validate = train_test_split(train_validate, test_size=0.1765, 
+                                       random_state=seed)
+    return train, validate, test
